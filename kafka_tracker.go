01788b569ea9cdc116e306dcfbb14a6ef9c1392f
Benedikt Böhm <bb@xnull.de>
2017-04-19 08:29:48 +0000

    do not use disk queue for safe messages

--- kafka_tracker.go
+++ kafka_tracker.go
@@ -1,23 +1,15 @@
 package tracker
 
 import (
-	"bytes"
 	"fmt"
-	"time"
 
 	"github.com/Shopify/sarama"
-	"github.com/beeker1121/goque"
 	"github.com/bobziuchkovski/cue"
 )
 
 // KafkaTracker is a tracker that sends messages to Apache Kafka.
 type KafkaTracker struct {
 	BaseTracker
-
-	queue *goque.Queue
-	quit  chan bool
-	done  chan bool
-
 	kafka struct {
 		fast sarama.AsyncProducer
 		safe sarama.SyncProducer
@@ -78,28 +70,11 @@ func NewKafkaTracker(
 		return nil, err
 	}
 
-	t.queue, err = goque.OpenQueue("cache/" + config.ClientID)
-	if err != nil {
-		_ = log.Error(t.kafka.fast.Close(), "failed to close fast producer")
-		_ = log.Error(t.kafka.safe.Close(), "failed to close safe producer")
-		return nil, err
-	}
-
-	t.quit = make(chan bool)
-	t.done = make(chan bool)
-
-	go t.start()
-
 	return t, nil
 }
 
 // Close the tracker.
 func (t *KafkaTracker) Close() {
-	// shutdown background worker
-	log.Info("closing tracker")
-	close(t.quit)
-	<-t.done
-
 	// shutdown safe producer
 	log.Info("closing safe producer")
 	_ = log.Error(t.kafka.safe.Close(), "failed to close safe producer")
@@ -107,10 +82,6 @@ func (t *KafkaTracker) Close() {
 	// shutdwn fast producer
 	log.Info("closing fast producer")
 	_ = log.Error(t.kafka.fast.Close(), "failed to close fast producer")
-
-	// close access to disk queue
-	log.Info("closing disk queue")
-	_ = log.Error(t.queue.Close(), "failed to close disk queue")
 }
 
 // FastMessage sends a message without waiting for confirmation.
@@ -140,60 +111,15 @@ func (t *KafkaTracker) SafeMessage(topic string, message interface{}) error {
 		return err
 	}
 
-	return t.enqueue(topic, buf)
-}
-
-// fail-safe disk queue worker
-
-var safeQueueDelim = []byte{0x0}
-
-func (t *KafkaTracker) enqueue(topic string, value []byte) error {
 	log.WithFields(cue.Fields{
 		"topic":   topic,
-		"message": string(value),
-	}).Debug("enqueue safe message")
-
-	msg := append([]byte(topic), safeQueueDelim...)
-	msg = append(msg, value...)
-	_, err := t.queue.Enqueue(msg)
-	return err
-}
-
-func (t *KafkaTracker) processSafeMessage(msg []byte) error {
-	idx := bytes.Index(msg, safeQueueDelim)
-	topic := string(msg[0:idx])
-	value := msg[idx+1:]
-
-	log.WithFields(cue.Fields{
-		"topic":   topic,
-		"message": string(value),
+		"message": string(buf),
 	}).Debug("sending safe message")
 
-	_, _, err := t.kafka.safe.SendMessage(&sarama.ProducerMessage{
+	_, _, err = t.kafka.safe.SendMessage(&sarama.ProducerMessage{
 		Topic: topic,
-		Value: sarama.ByteEncoder(value),
+		Value: sarama.ByteEncoder(buf),
 	})
 
 	return err
-}
-
-func (t *KafkaTracker) start() {
-	for {
-		select {
-		default:
-			item, err := t.queue.Dequeue()
-			if err == goque.ErrEmpty {
-				time.Sleep(100 * time.Millisecond)
-			} else if err != nil {
-				log.Panic(err, "unknown queue error")
-			} else if err = t.processSafeMessage(item.Value); err != nil {
-				_ = log.Error(err, "failed to process safe message")
-				_, err = t.queue.Enqueue(item.Value)
-				_ = log.Error(err, "failed to enqueue safe message")
-			}
-		case <-t.quit:
-			close(t.done)
-			return
-		}
-	}
 }
