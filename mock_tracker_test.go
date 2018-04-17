7d4334c41012b6f47cb18696f49e653fbe3a28fe
Georgy Buranov <gburanov@gmail.com>
2018-04-17 10:35:11 +0200

    changes

--- mock_tracker_test.go
+++ mock_tracker_test.go
@@ -6,21 +6,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func trackerWithSampleData(topic string, t *testing.T) *MockTracker {
-	metadata := EventMetadata{}
-	subject := NewMockTracker(&metadata)
+func TestMockTrackerBasic(t *testing.T) {
+	topic := "topic"
+	subject := NewMockTracker(&EventMetadata{})
 	err := subject.FastMessage(topic, []byte("message1"))
 	require.Nil(t, err)
 	err = subject.FastMessage(topic, []byte("message2"))
 	require.Nil(t, err)
 	err = subject.FastMessageWithKey(topic, []byte("message3"), []byte("key3"))
 	require.Nil(t, err)
-	return subject
-}
-
-func TestMockTrackerBasic(t *testing.T) {
-	topic := "topic"
-	subject := trackerWithSampleData(topic, t)
 
 	value := subject.Get(topic, 0)
 	require.Equal(t, "message1", string(value))
@@ -34,20 +28,33 @@ func TestMockTrackerBasic(t *testing.T) {
 
 func TestMockTrackerTopicIteration(t *testing.T) {
 	topic := "topic"
-	subject := trackerWithSampleData(topic, t)
+	subject := NewMockTracker(&EventMetadata{})
+	err := subject.FastMessage(topic, []byte("message1"))
+	require.Nil(t, err)
+	err = subject.FastMessage(topic, []byte("message2"))
+	require.Nil(t, err)
+	err = subject.FastMessageWithKey(topic, []byte("message3"), []byte("key3"))
+	require.Nil(t, err)
 	it := subject.Iterate(topic)
 	// Check iteration
 	key, value, canContinue := it.Next()
 	require.Nil(t, key)
 	require.Equal(t, "message1", string(value))
 	require.True(t, canContinue)
+
 	key, value, canContinue = it.Next()
 	require.Nil(t, key)
 	require.Equal(t, "message2", string(value))
 	require.True(t, canContinue)
+
 	key, value, canContinue = it.Next()
 	require.Equal(t, "key3", string(key))
 	require.Equal(t, "message3", string(value))
+	require.False(t, canContinue)
+
+	key, value, canContinue = it.Next()
+	require.Nil(t, key)
+	require.Nil(t, value)
 	require.False(t, canContinue)
 }
 
